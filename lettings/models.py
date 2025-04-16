7e5b4626f2d3bd4ad974037413c09677978af0f2
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-04-16 11:43:54 +0200

    create profiles app and do all migrations

--- lettings/models.py
+++ lettings/models.py
@@ -10,6 +10,9 @@ class Address(models.Model):
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
+    class Meta:
+        db_table = "oc_lettings_site_address"
+
     def __str__(self):
         return f'{self.number} {self.street}'
 
@@ -17,6 +20,9 @@ class Address(models.Model):
 class Letting(models.Model):
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "oc_lettings_site_letting"
 
     def __str__(self):
         return self.title
--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-
-from lettings.models import Letting, Address
-from .models import Profile
-
-
-admin.site.register(Letting)
-admin.site.register(Address)
-admin.site.register(Profile)
