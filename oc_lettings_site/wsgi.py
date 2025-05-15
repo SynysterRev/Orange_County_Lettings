388f42f0d2b830987f2d3f9625f03d7e057e50d2
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-05-15 15:54:27 +0200

    correct yml for test

--- oc_lettings_site/wsgi.py
+++ oc_lettings_site/wsgi.py
@@ -2,6 +2,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oc_lettings_site.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      os.getenv('DJANGO_SETTINGS_MODULE',
+                                'oc_lettings_site.settings.production'))
 
 application = get_wsgi_application()
