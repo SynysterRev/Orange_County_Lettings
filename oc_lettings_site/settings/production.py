80d48518bdbd56abdff978885e720a2bd488ecb6
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-05-19 14:21:23 +0200

    add allowed host

--- oc_lettings_site/settings/production.py
+++ oc_lettings_site/settings/production.py
@@ -1,13 +1,9 @@
-import sentry_sdk
-
-import os
 from .base import *  # noqa: F401, F403
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "*"]
+ALLOWED_HOSTS = ["https://orange-county-lettings-5vld.onrender.com/"]
 
 SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
@@ -18,6 +14,8 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
 SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
+
+CSRF_TRUSTED_ORIGINS = ['https://orange-county-lettings-5vld.onrender.com']
 
 sentry_sdk.init(
     dsn=os.getenv('SENTRY_DSN'),
