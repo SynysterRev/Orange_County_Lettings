9b283d9f46ff8714f801b721789d7c9a2c85d6be
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-05-20 09:40:25 +0200

    update allowed hosts

--- oc_lettings_site/settings/production.py
+++ oc_lettings_site/settings/production.py
@@ -3,7 +3,7 @@ from .base import *  # noqa: F401, F403
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["orange-county-lettings-5vld.onrender.com"]
+ALLOWED_HOSTS = ["orange-county-lettings-l543.onrender.com"]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -17,4 +17,4 @@ SECURE_HSTS_PRELOAD = True
 
 SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
 
-CSRF_TRUSTED_ORIGINS = ['https://orange-county-lettings-5vld.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://orange-county-lettings-l543.onrender.com']
