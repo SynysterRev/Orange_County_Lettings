7aaae04fb2a28f0064c4d171994a6b05fda3e32e
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-06-02 13:54:40 +0200

    remove comments

--- oc_lettings_site/settings/test.py
+++ oc_lettings_site/settings/test.py
@@ -1,9 +1,7 @@
 from .base import *  # noqa: F401, F403
 
-# Désactivation des paramètres de sécurité pour les tests
 DEBUG = True
 
-# Désactivation des redirections SSL pour les tests
 SECURE_SSL_REDIRECT = False
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
@@ -12,7 +10,6 @@ SECURE_HSTS_SECONDS = 0
 SECURE_HSTS_INCLUDE_SUBDOMAINS = False
 SECURE_HSTS_PRELOAD = False
 
-# Utiliser une base de données en mémoire pour les tests rapides
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -20,7 +17,6 @@ DATABASES = {
     }
 }
 
-# Configuration de mot de passe simplifiée pour accélérer les tests
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
 ]
