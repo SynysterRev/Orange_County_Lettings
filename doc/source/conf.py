5cf11dedfbf6f5bba3a5baff622c791ebbaf5753
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-06-02 14:42:30 +0200

    update read the docs conf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import django
+import sphinx_rtd_theme
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -32,6 +33,7 @@ language = 'fr'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 sys.path.insert(0, os.path.abspath('../..'))
